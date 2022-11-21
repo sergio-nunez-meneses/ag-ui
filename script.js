0d0dadf5957d05126e4e7f802e95e1895d5b1a96
sergio núñez meneses <snunezmeneses@gmail.com>
2022-11-21 19:42:56 +0100

    WIP drop target sound file

--- script.js
+++ script.js
@@ -5,10 +5,22 @@
 // ============================================================================
 //  Variables
 // ============================================================================
+let soundFilesDragOver = false;
 
 // ============================================================================
 //  Functions
 // ============================================================================
+function scanFiles(item) {
+	if (item.isDirectory) {
+		item.createReader().readEntries((entries) => {
+			console.log(`Uploaded files: ${entries.length}`);
+
+			entries.forEach((entry) => {
+				scanFiles(entry);
+			});
+		});
+	}
+}
 
 // ============================================================================
 //  Code to execute
@@ -17,3 +29,26 @@
 // ============================================================================
 //  EventListeners
 // ============================================================================
+document.querySelector(".drop-zone").addEventListener("dragover", (e) => {
+	e.preventDefault();
+
+	if (!soundFilesDragOver) {
+		console.log(`Dragging ${e.target.id}`);
+
+		soundFilesDragOver = true;
+	}
+})
+document.querySelector(".drop-zone").addEventListener("drop", (e) => {
+	e.preventDefault();
+
+	console.log(`Dropped ${e.target.id}:`);
+
+	// If corpus ?
+	for (let item of e.dataTransfer.items) {
+		if (item.webkitGetAsEntry()) {
+			scanFiles(item.webkitGetAsEntry());
+		}
+	}
+
+	soundFilesDragOver = false;
+})
