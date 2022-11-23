6e31e1497b2d8a96833e09de25256f97f9101e29
sergio núñez meneses <snunezmeneses@gmail.com>
2022-11-23 10:29:47 +0100

    WIP add functions getFiles and readEntryContent in script.js

--- script.js
+++ script.js
@@ -10,22 +10,59 @@ let soundFilesDragOver = false;
 // ============================================================================
 //  Functions
 // ============================================================================
-function scanFiles(item) {
-	let dt = new DataTransfer();
+async function getFiles(dataTransfer) {
+	const files = [];
 
-	if (item.isDirectory) {
-		item.createReader().readEntries((entries) => {
-			if (entries.length > 0) {
-				for (let entry of entries) {
-					entry.file((file) => {
-						dt.items.add(file);
-					})
-				}
-			}
-		});
+	for (const item of dataTransfer.items) {
+		if (item.kind.split('/')[0] !== "audio") {
+			// End function execution
+		}
 
-		return dt.files;
+		if (item.webkitGetAsEntry()) {
+			const entry = item.webkitGetAsEntry();
+			const entryContent = await readEntryContent(entry);
+
+			files.push(...entryContent);
+		}
 	}
+	return files;
+}
+
+function readEntryContent(entry) {
+    return new Promise((resolve, reject) => {
+        const contents = [];
+    	let counter = 0;
+
+        readEntry(entry);
+
+        function readEntry(entry) {
+        	counter++;
+
+        	if (entry.isDirectory) {
+	        	entry.createReader().readEntries((entries) => {
+	        		counter--;
+
+	        		for (const entry of entries) {
+	        			readEntry(entry);
+	        		}
+
+	        		if (counter === 0) {
+	        			resolve(contents);
+	        		}
+	            });
+        	}
+            else {
+            	entry.file((file) => {
+            		counter--;
+            		contents.push(file);
+
+            		if (counter === 0) {
+            			resolve(contents);
+            		}
+            	});
+            }
+        }
+    });
 }
 
 // ============================================================================
@@ -44,14 +81,11 @@ document.querySelector(".drop-zone").addEventListener("dragover", (e) => {
 		soundFilesDragOver = true;
 	}
 })
-document.querySelector(".drop-zone").addEventListener("drop", (e) => {
+document.querySelector(".drop-zone").addEventListener("drop", async (e) => {
 	e.preventDefault();
 
 	console.log(`Dropped ${e.target.id}:`);
-
-	for (item of e.dataTransfer.items) {
-		console.log(await scanFiles(item.webkitGetAsEntry()));
-	}
+	console.log(await getFiles(e.dataTransfer));
 	
 	soundFilesDragOver = false;
 })
